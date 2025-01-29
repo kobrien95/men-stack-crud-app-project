d51b335b833d6a27a28488669b2a261f44d2dd90
KrisO95 <kristianobrien95@gmail.com>
2025-01-28 21:03:15 -0500

    added show page as well as delete feature to webpage

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -10,6 +10,51 @@ router.get('/new', async (req, res) => {
 
 // -----------------------------------
 
+router.get('/:shoeId/edit', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      const shoe = currentUser.shoes.id(req.params.shoeId);
+      res.render('shoes/edit.ejs', {
+        shoe: shoe,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+// -----------------------------------
+
+router.get('/:shoeId', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      const shoe = currentUser.shoes.id(req.params.shoeId);
+      res.render('shoes/show.ejs', {
+        shoe: shoe,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
+
+// -----------------------------------
+
+router.delete('/:shoeId', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.shoes.id(req.params.shoeId).deleteOne();
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/shoes`);
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
+// -----------------------------------
+
 router.get('/', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id);
